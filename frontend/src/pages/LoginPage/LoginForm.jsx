4e734d74199b334327ceee41c36942a19c0c81db
Alex Near <piroxelcodes@gmail.com>
2026-06-22 12:57:35 -0400

    Began frontend development Created rough draft of login page layout and structured project folders

---
+++ frontend/src/pages/LoginPage/LoginForm.jsx
@@ -0,0 +1,31 @@
+import '../LoginPage/LoginForm.css'
+import placeholder from '../../assets/Rectangle.png'
+export default function LoginForm(){
+    return ( <div id='login-container'>
+    
+        <div id='login-form'>
+            <form>
+                <fieldset>
+                    <legend>
+                        Please enter your school credentials
+                    </legend>
+                    <label htmlFor="f-email"> 
+                        School Email
+                    </label>
+                    <input type="text" id="f-email"/>
+                    <label htmlFor="f-password"> 
+                        Password
+                    </label>
+                    <input type="text" id="f-password"/>
+                    <a href="">Forgot my password</a>
+                    <div id = "button-row">
+                    <button type="submit" id="login-student">Sign In As Student</button>
+                    <button type="submit" id="login-admin">Sign In As Admin</button>
+                    </div>
+                </fieldset>
+            </form>
+            <img id="login-image" src={placeholder}/>
+        </div>
+    </div>
+    );
+}
